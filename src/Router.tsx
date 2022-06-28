55cbb05d4147edc123155f7d4d77f37f19abc932
André Souza <als260602@msn.com>
2022-06-28 08:44:10 -0300

    intgrating graphql codegen

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Event } from "./pages/Event";
-import { Subscripe } from "./pages/Subscripe";
+import { Subscribe } from "./pages/Subscribe";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Subscripe />} />
+      <Route path="/" element={<Subscribe />} />
       <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,23 +1,15 @@
-import { gql, useMutation } from "@apollo/client";
 import React, { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Header/Logo";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
-const CREATE_SUBCRIBE_MUTATION = gql`
-  mutation CreateSubscriber($name: String!, $email: String!) {
-    createSubscriber(data: { name: $name, email: $email }) {
-      id
-    }
-  }
-`;
-
-export const Subscripe = () => {
+export const Subscribe = () => {
   const navigate = useNavigate();
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
-  const [createSubscriber, { loading }] = useMutation(CREATE_SUBCRIBE_MUTATION);
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
   const handleSubscribe = async (event: FormEvent) => {
     event.preventDefault();
